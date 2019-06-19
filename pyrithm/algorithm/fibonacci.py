b1a32956517e2a9dda8be6822751abb2999d14a8
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-06-19 03:46:13 -0700

    fibonacci still has a bug even though it was restructured a little bit. need to think through the iterative solution more carefully.

--- pyrithm/algorithm/fibonacci.py
+++ pyrithm/algorithm/fibonacci.py
@@ -21,24 +21,42 @@ class Fibonacci():
     
     def by_iteration_alpha(self, target_n: int) -> int:
         """Return the nth element of the Fibonacci sequence by iteratively calculating it."""
-        # We call the argument target_n so we can refer to n in comments without confustion.
-        # The zeroeth element is a special edge case when calculating iteratively in this manner.
+        # We call the argument target_n so we can refer to n in comments without confusion.
+        # The n=0 element is a special edge case when calculating iteratively in this manner.
         if target_n == 0:
             return 0
-        iteration = 0
-        previous_member = 0
+        # The n=1 element is also a special edge case because we need at least 2 members present
+        # in the sequence, in order for the loop to operate below as currently designed.
+        # This is only the first implementation method, hence it is marked _alpha. We will
+        # explore other implementation methods in this class and the preferred method will
+        # be aliased to the symbol 'member' so we can always call the preferred implementation
+        # via member(n). The alias is made at the end of the different implementation method
+        # definitions.
+        if target_n == 1:
+            return 1
+        # HACK:
+        #iteration = 0
+        # The following is a hack in the sense that I observed output and realized that the
+        # sequence was missing the n=1 member, but had the n=0 member correct (form the edge
+        # case code) and had all sequence members correct from n=2 and onwards. So by deduction
+        # concluded I needed to include an addition edge case for n=1 and also by deduction I
+        # realized I needed to shift the iteration counter back one to make things work.
+        # I do not want to leave things like this. I want to have a clearly understood design,
+        # not one made to work be a hack deduced from output, even if the design ends up
+        # identical, I want to fully understand my original mistake and make all the comments
+        # and variable names as correct as possible according to a well thought out design.
+        # The hack/fix will be applied on the next commit so this can be easily seen in code diffs.
+        iteration = 1
+        previous_member = 0  # Initialization value, not really a previous member
         # To use the loop below we also need to handle an edge case of sorts and initlize
         # the current_current member which is the n=1 member as we begin the iterations.
         # And the n=1 member of course has a value of 1.
         current_member = 1
-        while not iteration == target_n:
-            iteration += 1  # This is effectively n as we progress from this point on.
-            # Often counters used to control a while loop are incremented at the end of the
-            # loops operations, but the current design requires we do it mostly becuse of
-            # the conditional used to control this loop.
+        while not iteration == target_n+1:
             sum = current_member + previous_member
             previous_member = current_member
             current_member = sum
+            iteration += 1
         
         return current_member
 
