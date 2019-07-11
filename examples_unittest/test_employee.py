797bdc998e6cf0a48c12d070d6327342b7e5923b
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2019-07-10 17:31:22 -0700

    use set/teardown with class-level example also

--- examples_unittest/test_employee.py
+++ examples_unittest/test_employee.py
@@ -8,41 +8,55 @@ from employee import Employee  # Class to be tested
 
 class TestEmployee(unittest.TestCase):
 
+    @classmethod
+    # NOTE: The unittest package uses some camel-case names.
+    # This is not best-practice and you should avoid it in your own code.
+    def setUpClass(cls):
+        print('setUpClass\n')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass\n')
+
+    def setUp(self):
+        """Create employee instances used by all tests"""
+        print('setUp')  # These prints are for illustrative purposes.
+        self.emp_1 = Employee('Jimmy', 'Gizmo', 80000)
+        self.emp_2 = Employee('Karen', 'Smith', 90000)
+
+    def tearDown(self):
+        print('tearDown\n')
+        pass
+
     def test_email(self):
-        emp_1 = Employee('Jimmy', 'Gizmo', 80000)
-        emp_2 = Employee('Karen', 'Smith', 90000)
+        print('test_email')
+        self.assertEqual(self.emp_1.email, 'Jimmy.Gizmo@email.com')
+        self.assertEqual(self.emp_2.email, 'Karen.Smith@email.com')
 
-        self.assertEqual(emp_1.email, 'Jimmy.Gizmo@email.com')
-        self.assertEqual(emp_2.email, 'Karen.Smith@email.com')
+        self.emp_1.first = 'Larry'
+        self.emp_2.first = 'Jane'
 
-        emp_1.first = 'Larry'
-        emp_2.first = 'Jane'
-
-        self.assertEqual(emp_1.email, 'Larry.Gizmo@email.com')
-        self.assertEqual(emp_2.email, 'Jane.Smith@email.com')
+        self.assertEqual(self.emp_1.email, 'Larry.Gizmo@email.com')
+        self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
 
     def test_fullname(self):
-        emp_1 = Employee('Jimmy', 'Gizmo', 80000)
-        emp_2 = Employee('Karen', 'Smith', 90000)
+        print('test_fullname')
+        self.assertEqual(self.emp_1.fullname, 'Jimmy Gizmo')
+        self.assertEqual(self.emp_2.fullname, 'Karen Smith')
 
-        self.assertEqual(emp_1.fullname, 'Jimmy Gizmo')
-        self.assertEqual(emp_2.fullname, 'Karen Smith')
+        self.emp_1.first = 'Larry'
+        self.emp_2.first = 'Jane'
 
-        emp_1.first = 'Larry'
-        emp_2.first = 'Jane'
-
-        self.assertEqual(emp_1.fullname, 'Larry Gizmo')
-        self.assertEqual(emp_2.fullname, 'Jane Smith')
+        self.assertEqual(self.emp_1.fullname, 'Larry Gizmo')
+        self.assertEqual(self.emp_2.fullname, 'Jane Smith')
 
     def test_apply_raise(self):
-        emp_1 = Employee('Jimmy', 'Gizmo', 80000)
-        emp_2 = Employee('Karen', 'Smith', 90000)
+        print('test_apply_raise')
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
 
-        emp_1.apply_raise()
-        emp_2.apply_raise()
-
-        self.assertEqual(emp_1.salary, 84000)
-        self.assertEqual(emp_2.salary, 94500)
+        self.assertEqual(self.emp_1.salary, 84000)
+        self.assertEqual(self.emp_2.salary, 94500)
 
 
 if __name__ == "__main__":
