9a24c921f52f894d680e3bb0c870b4add85ee49f
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-10-09 21:39:40 -0700

    moved prime number cache into instances

--- examples_pyrithm/prime_module_instance_ex.py
+++ examples_pyrithm/prime_module_instance_ex.py
@@ -2,46 +2,77 @@
 
 import pyrithm.algorithm.prime as prime
 
-VERBOSE = True
+VERBOSE = False
 
 print("\n")
-print(f"This example creates an instance of the Prime class, in contrast to the static example.")
-print(f"When instantiating an instance, we can specify a maximum value up to which to pre-compute prime number"
-      f"values.")
+print(f"This example script creates an instance of the Prime class, in contrast to the static example which just "
+      f"uses static methods within the class (see prime_numbers_ex.py).")
+print(f"Each instance of Prime can have its own cache of pre-computed prime numbers for a performance boost over "
+      f"that range. This is a generic concept for you to learn from and adapt to other OOP use-cases.")
+print(f"When creating an instance, we can specify the maximum value up to which to pre-compute (search for) "
+      f"prime number values.")
+print()
+
+print("Created an instance with a small pre_compute of 30.")
+prime_class_instance_30 = prime.Prime(30)
+print()
+
+print("Created an instance with a medium pre_compute of 100.")
+prime_class_instance_100 = prime.Prime(100)
+print()
+
+print("Created an instance with a large pre_compute of 1000.")
+prime_class_instance_1000 = prime.Prime(1000)
+print()
+
+print("Created an instance with an extra-large pre_compute of 40000. Hold on a sec for this one ...")
+prime_class_instance_20000 = prime.Prime(40000)
+print("Whew! That one actually took a noticeable amount of hard work by the CPU for a few seconds there.")
+print()
+
+
+print("Looking closer at the instance with the pre_compute of 1000; Let's direct;y access this cache and have"
+      "a look. \n")
+print("How many actual prime numbers are in this cache? Lets count the cache entries:")
+print(len(prime_class_instance_1000.cache))  # IDEs may warn here about accessing a protected class member.
+print(f"What is the last and therefore highest cache entry (prime number): "
+      f"{prime_class_instance_1000.cache[-1]}")
 print("\n")
 
-prime_class_instance = prime.Prime(100)
+
+print("Let's just see the entire internal cache in this instance. It is just a list, like an array.\n")
+print(prime_class_instance_1000.cache)
 print("\n")
 
-prime_class_instance_smaller_pcomp = prime.Prime(40)
+
+print("Now we'll recreate the same example as the static usage in the other script, but call the is_prime() method "
+      "through the instance we created.\n")
+print(f"n  All prime numbers between zero and 200, inclusive.")
+print(f"-  --------------------------------------------------")
+
+maximum = 200
+for n in range(1, maximum + 1):  # Plus 1 so we include the maximum value itself.
+    if VERBOSE:
+        print(f"{n}  { 'PRIME' if prime_class_instance_1000.is_prime(n) else '- - -' }")
+    else:
+        if prime_class_instance_1000.is_prime(n):  # A static method called through an instance.
+            print(n, end="")
+            if n < maximum - 1:  # This is just for proper comma formatting as the processing output completes.
+                print(", ", end="")
+            else:
+                print()
+
 print("\n")
 
-prime_class_instance_larger_pcomp = prime.Prime(200)
-print("\n")
+# For the non-verbose output, we want comma-separated values on a single line with no extra comma on the end.
+# We could easily get something like this by pre-calculating everything into an array and then just printing the array.
+# However, we don't want to require pre-calculation and thus waiting in this case (regardless of how fast or slow)
+# so the way we did it, we will output as the numbers are calculated, but will still have the proper formatting
+# for the last item. There are almost always edge-cases to handle in every requirement.
+
 
 # Try an invalid pre_compute value. It should throw an exception as a ValueError.
-prime_class_instance_invalid_pcomp = prime.Prime(1)
+print("Invalid pre_compute of 1. Intentional ERROR follows:\n")
+prime_class_instance__invalid_pre_compute = prime.Prime(1)
 print("\n")
 
-# print(prime_class_instance._cache_max_pre_compute)
-#
-# print(f"n  All prime numbers between zero and 200, inclusive.")
-# print(f"-  --------------------------------------------------")
-#
-# maximum = 200
-# for n in range(1, maximum + 1):  # Plus 1 so we include the maximum value itself.
-#     if VERBOSE:
-#         print(f"{n}  { 'PRIME' if prime.Prime.is_prime(n) else '- - -' }")
-#     else:
-#         if prime.Prime.is_prime(n):  # This is a static use. A class static method is called directly.
-#             print(n, end="")
-#         if n < maximum:
-#             print(", ", end="")
-# print()
-#
-# # For the non-verbose output, we want comma-separated values on a single line with no extra comma on the end.
-# # We could easily get something like this by pre-calculating everything into an array and then just printing the array.
-# # However, we don't want to require pre-calculation and thus waiting in this case (regardless of how fast or slow)
-# # so the way we did it, we will output as the numbers are calculated, but will still have the proper formatting
-# # for the last item. There are almost always edge-cases to handle in every requirement.
-#
--- pyrithm/algorithm/prime.py
+++ pyrithm/algorithm/prime.py
@@ -2,16 +2,18 @@
 
 class Prime:
     """
-    Generate a sequence of prime numbers. Determine if an integer is a prime number.
-    A prime number is a whole number greater than 1 whose only factors are 1 and itself.
-    The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23, 29.
+    Provide utility methods to determine of a number is a prime number. Instances of Prime can be created with
+    a cache of pre-computed prime numbers over a specified range for a performance enhancement.
+    Optional ways to use these features are provided.
+    The first 10 prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23, 29.
     """
-    VERBOSE = True
+    VERBOSE = False
+    DEBUG = False
 
     # Optional cache of pre-calculated results such as what pyrithm.algorithm.fibonacci has.
     # There are many use-cases where caching a pre-computed value is helpful, even for relatively simple calculations.
-    _cache = []
-    _cache_max_pre_compute = 0  # This is not the max prime value in the cache. This is the largest pre_compute value.
+    #_cache = []  # These class variables will no longer be used. Changing to per-instance caches.
+    #_cache_max_pre_compute = 0  # This is not the max prime value in the cache. This is the largest pre_compute value.
 
     @staticmethod
     def is_divisible_by(dividend, divisor):
@@ -21,10 +23,11 @@ class Prime:
     def is_prime(x: int) -> bool:
         if x < 2:  # Edge case because 1 is not a prime number but would pass our algorithmic checks below.
             return False
-        # We cannot check all the way up to x because x/x we know is 1 and is allowed for prime numbers. Stop at x-1.
-        for divisor in range(2, x - 1):  # I believe we can stop checking earlier, but exactly where? x/2? (x/2)+1?
+        # The second argument to range() is not included in the range and this works well, because we already
+        # know that x is divisible by x exactly one time, and so we do not need to check it.
+        for divisor in range(2, x):  # I believe we can stop checking earlier, but exactly where? x/2? (x/2)+1?
             if Prime.is_divisible_by(x, divisor):
-                if Prime.VERBOSE:
+                if Prime.DEBUG:
                     print(f"{divisor} is a factor of {x} so {x} cannot be a prime number. "
                           f"No additional factors need to be tried.")
                 return False
@@ -33,30 +36,25 @@ class Prime:
     def __init__(self, pre_compute: int):
         """
         Initialize an instance of Prime. If a pre_compute integer value is given, look for all prime numbers
-        that may exist between 0 and pre_compute and save all of those prime numbers in the Prime class cache.
-        This cache is available to all instances. The cache will be as large as the largest pre_compute value
-        requested by any instance, thus it can serve all the instances. If the cache is already large enough,
-        no additional pre-computing of prime numbers will be performed. Values up to and including the
-        pre_cache value itself will be included when they are a prime number. (Of course this is all educational,
-        as I don't see a huge need for instances of this class in practice, but it is a good illustrative case.
-        Prime number code could work fine as a static class or a static module with no OO at all, but I am
-        trying to cover many Python topics with the Pyrithm library and provide a lot of good code and design
-        patterns which can be reused.)
+        that may exist between 0 and pre_compute and save all of those prime numbers in the instance's cache.
+        Values up to and including the pre_cache value itself will be included when they are a prime number.
+        # TODO: Verify the 'up to and including' aspect.
         """
+        self.cache = []
+        self.cache_max_pre_compute = pre_compute  # Doesn't seem like we need this now with instance-level cache.
+
         if pre_compute < 2:
             raise ValueError("The pre_compute constructor argument of Prime() must be an integer > 1.")
 
-        if pre_compute > Prime._cache_max_pre_compute:
-            if Prime.VERBOSE:
-                print(f"Pre-populating class-level prime number cache for pre_compute value: {pre_compute}")
-            # TODO: This very dumbly rebuilds the entire cache. Make it start where it left off, if it can.
-            Prime._cache = []
-            for n in range(1, pre_compute + 1):
-                if Prime.is_prime(n):
-                    if Prime.VERBOSE:
-                        print(f"{n} is a prime number. Adding it to the cache.")
-                    Prime._cache.append(n)
+        if Prime.VERBOSE:
+            print(f"Pre-populating class-level prime number cache for pre_compute value: {pre_compute}")
+
+        for n in range(1, pre_compute + 1):
+            if Prime.is_prime(n):
+                if Prime.VERBOSE:
+                    print(f"{n} is a prime number. Adding it to the cache.")
+                self.cache.append(n)
         else:
             if Prime.VERBOSE:
-                print(f"No additional pre-computing needed. _cache_max_pre_compute: {Prime._cache_max_pre_compute}")
+                print(f"No additional pre-computing needed. _cache_max_pre_compute: {self.cache_max_pre_compute}")
 
