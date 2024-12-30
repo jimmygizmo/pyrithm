bd51056be08560d460019b3d1590710fd4cf551e
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2024-12-30 14:05:07 -0800

    added more unit tests. improved unit tests logging and included a decorator experiment to fill gaps in unittest verbosity options, but this decorator idea failed. its in here for reference and will remove next push leaving cut-n-paste equivalent for solving unittest output issue I have. (I want to see the test names along with all my verbose logging from the module itself) Also, I made the module verbose output more clear and took out the word fail for when terms are not found. (confuses with unit tests)

--- pyrithm/algorithm/search/binary.py
+++ pyrithm/algorithm/search/binary.py
@@ -1,7 +1,7 @@
 
 V_: bool = True
 
-
+# TODO: Subclass ?
 class BinarySearchIterative:
     """Binary search algorithm, iterative implementation, full-featured but simple. No special optimizations."""
 
@@ -12,41 +12,51 @@ class BinarySearchIterative:
             raise ValueError("You must initialize with a sorted integer list of at least one integer.")
 
     def search(self, term: int) -> int|None:
-        if V_: print(f"\nBinarySearchIterative running...    Search term: {term}\n"
+        if V_: print(f"BinarySearchIterative running...    Search term: {term}\n"
                      f"List element count (length): {len(self.s_list)}")
+        # TODO: Move these vars to instance vars?
         attempt: int = 0
         min_index: int = 0
         max_index: int = len(self.s_list) - 1
-        mid_index: int = (len(self.s_list) // 2) - 1
-        # TODO: Possibly:
-        # left_length: int = mid_index - min_index
-        # right_length: int = max_index - mid_index
-        # TODO: IMPORTANT! - If we can do these JUST inside the loop, we need not do them below at all ??!???!!?
-        # TODO: This looks doable and would be on a great path to cleaner, simpler code. (Our edge case prob still exists FYI
-        #            but it is nicer to solve it with already-simplified code. Might as well do more of this great refactoring
-        #            we are getting done before working hard on our 1 little remaining edge case. 46 diverse unit tests now pass. It's just the one..
+        # BIG SURPRISE!!! Things seem to be working but I see mid_index-related calcs are off. Attempting fix below here.
+        # mid_index: int = (len(self.s_list) // 2) - 1
+        mid_index: int = (len(self.s_list) // 2)  # GREAT ! This was a fix. Did not change that all tests are/were passing. BUT, finding suff in fewer attempts! Great.
+        # TODO: Trial fix for the latest edge case. Seems like we need this because without it we can successfully
+        #         exit with a found status for a nonexistent -1 index. This 1 element list [1] with search(1) triggers the final edge case.
+        if mid_index < min_index:
+            mid_index = min_index  # In this case, always 0
+        # TODO: UPDATE - That fixed it. NO MORE BUGS LEFT!
+        #
+        # Just need to initialize the lengths here and prior to first loop we don't really have a left/right, so we
+        # will initialize to the full size for both, as any logic we do only cares about when lengths approach zero.
+        # This is more correct especially for really small lists like one or two element lists. Our other option is to
+        # use None, but we need to watch out for creating an unnecessary need for limit checks when right here all we
+        # want to do is initialize these without creating some edge case that is a false exit condition for small lists.
+        left_length: int = max_index - min_index
+        right_length: int = max_index - min_index
+        selected_side: str|None = 'START'
         while True:
             attempt += 1
             if V_: print(f"ATTEMPT ({attempt}):  "
-                         f"min_index: {min_index}    mid_index: {mid_index}    max_index: {max_index}")
+                         f"min_index: {min_index}    mid_index: {mid_index}    max_index: {max_index}\n"
+                         f"side: {selected_side}    left_length: {left_length}    right_length: {right_length}")
 
             if self.s_list[mid_index] == term:
-                if V_: print(f"--FOUND-- the search term ({term}) at index: {mid_index}    Steps/attempts: {attempt}")
+                if V_: print(f"[* FOUND *] term ({term}) at index: {mid_index}    Steps/attempts: {attempt}")
                 return mid_index
 
-            # TODO: Plan to embody these more simply by using left_length and right_length and after those calcs
             if min_index == max_index:
-                if V_: print(f"--failed-- to find term.  Exit rule:  min_index == max_index")
+                if V_: print(f"[~ term NOT found ~] ({term})  Exit rule:  min_index == max_index")
                 return None
             if mid_index > max_index:
-                if V_: print(f"--failed-- to find term.  Exit rule:  mid_index > max_index")
+                if V_: print(f"[~ term NOT found ~] ({term})  Exit rule:  mid_index > max_index")
                 return None
             if mid_index < min_index:
-                if V_: print(f"--failed-- to find term.  Exit rule:  mid_index < min_index")
+                if V_: print(f"[~ term NOT found ~] ({term})  Exit rule:  mid_index < min_index")
                 return None
 
             if self.s_list[mid_index] < term:
-                right_length: int = max_index - mid_index  # --RIGHT-- side selected for next search step.
+                right_length = max_index - mid_index  # --RIGHT-- side selected for next search step.
                 if V_: print(f"   The term ({term}) > the mid_index element value ({self.s_list[mid_index]}).  "
                              f"Next search: RIGHT side. right_length: {right_length}")
                 new_right_min_index: int = mid_index + 1
@@ -57,9 +67,9 @@ class BinarySearchIterative:
                     new_right_mid_index = max_index
                 min_index = new_right_min_index
                 mid_index = new_right_mid_index
-                # TODO: Possibly maintain left_length and right_length in vars so we can use them for our exit checks.
+                selected_side = 'RIGHT'
             else:
-                left_length: int = mid_index - min_index  # --LEFT-- side selected for next search step.
+                left_length = mid_index - min_index  # --LEFT-- side selected for next search step.
                 if V_: print(f"   The term ({term}) < the mid_index element value ({self.s_list[mid_index]}).  "
                              f"Next search: LEFT side. left_length: {left_length}")
                 new_left_max_index: int = mid_index - 1
@@ -70,5 +80,5 @@ class BinarySearchIterative:
                     new_left_mid_index = min_index
                 max_index = new_left_max_index
                 mid_index = new_left_mid_index
-                # TODO: Possibly maintain left_length and right_length in vars so we can use them for our exit checks.
+                selected_side = 'LEFT'
 
--- pyrithm/tests/test_binary_search.py
+++ pyrithm/tests/test_binary_search.py
@@ -2,116 +2,178 @@
 
 import unittest
 # https://docs.python.org/3/library/unittest.html
+
+import inspect  # So we can inspect the stack and determine the name of each function for helpful logging.
+
 import pyrithm.algorithm.search.binary as bsearch  # Local module to be tested
 
+
+V_ = True  # Verbose logging switch
+
 bsearch = bsearch.BinarySearchIterative  # TODO: Also test the recursive version, later when we have it.
+
+# NOTE REGARDING:     inspect.stack()[0][3]
+# This is a trick to see the name of the function/test in a convenient manner. I also use configurable verbose
+# output from the module iteself and this technique intersperses that output with the descriptive test names. Of
+# course part of the value with all this comes from using good, descriptive test names. In my case, these test
+# function names are also hierarchically accurate.
+
+# SIDENOTE: If you want to see the functions CALLER:
+# inspect.stack()[1][3]
+# If we try to implement this more cleanly using a decorator, we might need this.
+# TODO: Try implementing these before and after agnostic prints as a decorator. It is a classic use case, but with
+# the added complexity of the inspect call needed to see the inner function, not the decorator wrapper function.
+# It might just work, or we might need to use the caller version, or this idea might not even work. If it does, it will
+# remove a LOT of repeated text from this file, so it would be nice to do.
+
+# Utility function in module space (not in the class). This is a decorator. TODO: Any reason to make it a class func?
+def function_headline(decorated_function):
+    """For helping unit tests show their names, show function name with ascii header and footer."""
+    def wrapper(*args, **kwargs):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[1][3]}")
+        result = decorated_function(*args, **kwargs)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
+        return result
+    return wrapper
+
+# TODO: UPDATE: The decorator idea is not working well with inspect but was worth a try and fun. This is what we get:
+# inspect.stack()[0][3]    says the function name is 'wrapper'
+# inspect.stack()[1][3]    sasys the funtion name is '_callTestMethod'
+# This makes sense .. of course we must be INSIDE the function to inspect it and decorators WRAP functions,
+# they don' infiltrate them. So this might not ever work with a decorator and it is not a critical feature. But there
+# might be some other way to look into later. Alas, this is all because the unittest module does not offer great
+# logging or verbosity options for excatly what I want to do in this case.
+
 
 class TestBinarySearch(unittest.TestCase):
 
     # 1 Element Tests
 
     def test_algorithm_search_binary__1e_tM1_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0   # Indices
             [1, ]  # 1 element, term (-1) @ index None
         ).search(-1), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__1e_t0_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0   # Indices
             [1, ]  # 1 element, term (0) @ index None
         ).search(0), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
-    # TODO: Our new edge case. Returns index -1. WEIRD!
     def test_algorithm_search_binary__1e_t1_i0(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0   # Indices
             [1, ]  # 1 element, term (1) @ index 0
         ).search(1), 0)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__1e_t2_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0   # Indices
             [1, ]  # 1 element, term (2) @ index None
         ).search(2), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
 
     # 2 Element Tests
 
     def test_algorithm_search_binary__2e_tM1_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1   # Indices
             [1, 2, ]  # 2 elements, term (-1) @ index None
         ).search(-1), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__2e_t0_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1   # Indices
             [1, 2, ]  # 2 elements, term (0) @ index None
         ).search(0), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__2e_t1_i0(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1   # Indices
             [1, 2, ]  # 2 elements, term (1) @ index 0
         ).search(1), 0)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__2e_t2_i1(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1   # Indices
             [1, 2, ]  # 2 elements, term (2) @ index 1
         ).search(2), 1)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
-    # THIS IS THE EDGE CASE FAILING WITH "list index out of range".
-    # UNIQUE FACTORS: The term is not in the list and is >> last element. It is a 2 element list and this means that
-    # the mid index will fall on min or max. NOW, for solution, focusing on how in the case of 2 elements, the // 2
-    # integer division results in the mid_index == max_index and so the left side has one element and the right side has none.
-    # SOLUTION ??? I don't know yet.
-    # TODO: UPDATE: Solved. Now we have only 1 edge case left .. a different one. And we exposed 2 others but solved all 3, creating a new one.
     def test_algorithm_search_binary__2e_t3_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1   # Indices
             [1, 2, ]  # 2 elements, term (3) @ index None
         ).search(3), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
 
     # 3 Element Tests
 
     def test_algorithm_search_binary__3e_t0_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1  2   # Indices
             [1, 2, 4, ]  # 3 elements, term (0) @ index None
         ).search(0), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__3e_t1_i0(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1  2   # Indices
             [1, 2, 4, ]  # 3 elements, term (1) @ index 0
         ).search(1), 0)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__3e_t2_i1(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1  2   # Indices
             [1, 2, 4, ]  # 3 elements, term (2) @ index 1
         ).search(2), 1)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__3e_t3_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1  2   # Indices
             [1, 2, 4, ]  # 3 elements, term (3) @ index None
         ).search(3), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__3e_t4_i2(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1  2   # Indices
             [1, 2, 4, ]  # 3 elements, term (4) @ index 2
         ).search(4), 2)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
     def test_algorithm_search_binary__3e_t5_iN(self):
+        if V_: print(f"\n- - - - - - - - - - - - - - - -  UNIT TEST: {inspect.stack()[0][3]}")
         self.assertEqual(bsearch(
             #0  1  2   # Indices
             [1, 2, 4, ]  # 3 elements, term (5) @ index None
         ).search(5), None)
+        if V_: print(f"- - - - - - - - - - - - - - - -")
 
 
     # 4 Element Tests
@@ -312,14 +374,151 @@ class TestBinarySearch(unittest.TestCase):
         ).search(12), None)
 
 
-    # High Element Tests
 
+    # Full Sequence Checks - 8 Elements: For finds and misses at all indexes
+    # For each position, tries all finds. Then tries all misses including before list and after list misses.
+
+    def test_algorithm_search_binary__8e_find0(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (1) @ index 0
+        ).search(1), 0)
+
+    def test_algorithm_search_binary__8e_find1(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (2) @ index 1
+        ).search(2), 1)
+
+    def test_algorithm_search_binary__8e_find2(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (3) @ index 2
+        ).search(3), 2)
+
+    def test_algorithm_search_binary__8e_find3(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (4) @ index 3
+        ).search(4), 3)
+
+    def test_algorithm_search_binary__8e_find4(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (5) @ index 4
+        ).search(5), 4)
+
+    def test_algorithm_search_binary__8e_find5(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (6) @ index 5
+        ).search(6), 5)
+
+    def test_algorithm_search_binary__8e_find6(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (7) @ index 6
+        ).search(7), 6)
+
+    def test_algorithm_search_binary__8e_find7(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (8) @ index 7
+        ).search(8), 7)
+
+
+    # 8 Elements - All misses with before, after misses. (These tests do in fact cover important code/traversal paths.)
+    # For the misses, the basic element pattern of 1, 2, 3, 4, 5, 6, 7, 8  is used to create the missing elements with
+    # help from the value 0, creating a gap which we move along from left to right for all these full-span miss tests.
+    # The test name for these indicates which index we are intentionally 'missing' for.
+    # A few of these tests are effectively repeats of a few we've already done, but it is better to be clear about
+    # the coverage and how the tests are composed and annotated here. Note that the sequence template for many of
+    # the early tests does not change (only its used portion does). But full sequencee tests in these sections use
+    # a changing sequence as the gap in values (so to speak) moves along for each subsequent test. Because of how
+    # processing traverses and can hit edge cases (limits) in many different scenarios (with a variety of exposure
+    # to different kinds of bugs, which additionally can only occur in a few unexpected edge cases) this is why we
+    # need very thorough coverage in unit tests for this module/algorithm. Also note, that we want to make a thorough
+    # comparison of different implementations in this project and even with other libraries.
+
+    def test_algorithm_search_binary__8e_missBeforeList(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (0) @ index None
+        ).search(0), None)
+
+    def test_algorithm_search_binary__8e_missForIdx0(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (1) @ index None
+        ).search(1), None)
+
+    def test_algorithm_search_binary__8e_missForIdx1(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (2) @ index None
+        ).search(2), None)
+
+    def test_algorithm_search_binary__8e_missForIdx2(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 2, 4, 5, 6, 7, 8, ]  # 8 elements, term (3) @ index None
+        ).search(3), None)
+
+    def test_algorithm_search_binary__8e_missForIdx3(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 2, 3, 5, 6, 7, 8, ]  # 8 elements, term (4) @ index None
+        ).search(4), None)
+
+    def test_algorithm_search_binary__8e_missForIdx4(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 2, 3, 4, 6, 7, 8, ]  # 8 elements, term (5) @ index None
+        ).search(5), None)
+
+    @function_headline
+    def test_algorithm_search_binary__8e_missForIdx5(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 2, 3, 4, 5, 7, 8, ]  # 8 elements, term (6) @ index None
+        ).search(6), None)
+
+    @function_headline
+    def test_algorithm_search_binary__8e_missForIdx6(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 2, 3, 4, 5, 6, 8, ]  # 8 elements, term (7) @ index None
+        ).search(7), None)
+
+    @function_headline
+    def test_algorithm_search_binary__8e_missForIdx7(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [0, 1, 2, 3, 4, 5, 6, 7, ]  # 8 elements, term (8) @ index None
+        ).search(8), None)
+
+    @function_headline
+    def test_algorithm_search_binary__8e_missAfterList(self):
+        self.assertEqual(bsearch(
+            #0  1  2  3  4  5  6  7   # Indices
+            [1, 2, 3, 4, 5, 6, 7, 8, ]  # 8 elements, term (9) @ index None
+        ).search(9), None)
+
+
+
+    # Higher Element Tests
+
+    @function_headline
     def test_algorithm_search_binary__18e_t21_i11(self):
         self.assertEqual(bsearch(
             #0  1  2  3  4  5  6  7   8   9  10  11  12  13  14  15  16  17     # Indices
             [1, 2, 4, 5, 6, 7, 8, 9, 11, 12, 15, 21, 22, 23, 25, 28, 29, 30, ]  # 18 elements, term (21) @ index 11
         ).search(21), 11)
         #
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
 
 
 ##
